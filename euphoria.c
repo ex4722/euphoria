b2741b4cc6ec093a9c5cd5d872827252bcf5bdaf
cosmo <cosmo@ebfe.rip@ex.ebfe.rip>
2024-06-03 10:03:54 -0400

    Initial commit

---
+++ euphoria.c
@@ -0,0 +1,26 @@
+/** 
+ * @file    euphoria.c 
+ * @author  Eddie
+ * @brief   Simple module to implemnt basic kernel functions like page walks and mmap handlers
+*/
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h> 
+  
+MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("Eddie"); 
+MODULE_DESCRIPTION("A simple utils kernel module"); 
+  
+static int __init hello_start(void) 
+{ 
+    pr_info("Loading euphoria module\n"); 
+    return 0; 
+} 
+  
+static void __exit hello_end(void) 
+{ 
+    pr_info("Euphoria Module exiting\n"); 
+} 
+  
+module_init(hello_start); 
+module_exit(hello_end); 
