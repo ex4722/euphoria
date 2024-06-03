94369764830490de00f16d1edca9a7f7a2d303bb
cosmo <cosmo@ebfe.rip@ex.ebfe.rip>
2024-06-03 10:33:30 -0400

    Adds Misc device initation

--- euphoria.c
+++ euphoria.c
@@ -5,23 +5,42 @@
 */
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/init.h> 
+#include <linux/init.h>
+#include "linux/fdtable.h"
+#include "linux/fs.h"
+#include "linux/highmem.h"
+#include "linux/miscdevice.h"
+
 #include "euphoria.h"
-  
-MODULE_LICENSE("GPL"); 
-MODULE_AUTHOR("Eddie"); 
-MODULE_DESCRIPTION("A simple utils kernel module"); 
-  
-static int __init hello_start(void) 
-{ 
-    pr_info("Loading euphoria module\n"); 
-    return 0; 
-} 
-  
-static void __exit hello_end(void) 
-{ 
-    pr_info("Euphoria Module exiting\n"); 
-} 
-  
-module_init(hello_start); 
-module_exit(hello_end); 
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Eddie");
+MODULE_DESCRIPTION("A simple utils kernel module");
+
+struct file_operations euphoria_fops = {
+    .owner = THIS_MODULE,
+};
+struct miscdevice euphoria_misc = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = "euphoria",
+    .fops =  &euphoria_fops,
+};
+
+static int __init euphoria_start(void){
+	pr_info("Loading euphoria module\n");
+    if(misc_register(&euphoria_misc) != 0){
+        pr_info("Unable to registger misc device\n");
+        return -1;
+    }else{
+        pr_info("Register Euphoria Misc Device Successful\n");
+        return 0;
+    }
+}
+
+static void __exit euphoria_end(void){
+    misc_deregister(&euphoria_misc);
+	pr_info("Euphoria Module Unloaded\n");
+}
+
+module_init(euphoria_start);
+module_exit(euphoria_end);
